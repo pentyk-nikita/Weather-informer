1211516855b2428751878af15f73ffc3683861d4
nikita <did430043@gmail.com>
2026-03-28 14:22:13 +0300

    KEY02dslmVpKLHXkkTupkvAD5NX1E1qhzsIiFfe7WvIugokT3GvD8RTCrVnJzXhqXVqupZok3349jh4cAB2o40uYHavQ/EOp+WVY4QPvJzJdNU=KEY

--- cmd/linux/cli/main.go
+++ cmd/linux/cli/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/pentyk-nikita/Weather-informer/internal/adapters/weather"
 	"github.com/pentyk-nikita/Weather-informer/internal/pkg/app/cli"
 	"github.com/pentyk-nikita/Weather-informer/pkg/cache"
 	"github.com/pentyk-nikita/Weather-informer/pkg/logger"
@@ -11,14 +12,14 @@ import (
 
 func main() {
 	l := logger.New()
-
+    wi := weather.New(l)
 	c, err := cache.New("./cache", 10*time.Minute)
 	if err != nil {
 		l.Error("Failed to create cache", err)
 		os.Exit(1)
 	}
 
-	app := cli.New(l, c)
+	app := cli.New(l, c, wi)
 
 	err = app.Run()
 	if err != nil {
--- internal/pkg/app/cli/app.go
+++ internal/pkg/app/cli/app.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/pentyk-nikita/Weather-informer/internal/domain/models"
 )
 
 type Logger interface {
@@ -15,6 +17,10 @@ type Logger interface {
 	Warn(string)
 }
 
+type WeatherInfo interface {
+	GetTemperature(float64, float64) models.TempInfo
+}
+
 type Cache interface {
 	Get(key string) ([]byte, bool)
 	Set(key string, data []byte) error
@@ -22,13 +28,15 @@ type Cache interface {
 
 type cliApp struct {
 	l     Logger
+	wi    WeatherInfo
 	cache Cache
 }
 
-func New(l Logger, cache Cache) *cliApp {
+func New(l Logger, cache Cache, wi WeatherInfo) *cliApp {
 	return &cliApp{
 		l:     l,
 		cache: cache,
+		wi:    wi,
 	}
 }
 
@@ -41,7 +49,7 @@ func (c *cliApp) Run() error {
 		Curr Current `json:"current"`
 	}
 
-	var response Response 
+	var response Response
 
 	cacheKey := fmt.Sprintf("weather_%.4f_%.4f", 53.6688, 23.8223)
 
@@ -105,6 +113,5 @@ func (c *cliApp) Run() error {
 		"Температура воздуха - %.2f градусов цельсия\n",
 		response.Curr.Temp,
 	)
-
 	return nil
 }
